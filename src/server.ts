c13609e3f43fe8b75d4044fe68fb98afa9420a5a
arogya <arogyabichpuria16@gmail.com>
2024-03-15 23:10:45 +0530

    added server initialization

---
+++ src/server.ts
@@ -0,0 +1,64 @@
+import "dotenv/config"; 
+import Fastify, {FastifyRequest , FastifyReply} from "fastify";
+import {withRefResolver} from "fastify-zod"; 
+import fjwt, {JWT} from "@fastify/jwt"; 
+import userRoutes from "./modules/user/user.routes";
+import {userSchemas} from "./modules/user/user.schema";
+
+
+declare module "fastify" {
+    interface FastifyRequest {
+      jwt: JWT;
+    }
+    export interface FastifyInstance {
+      authenticate: any;
+    }
+  }
+  
+  declare module "@fastify/jwt" {
+    interface FastifyJWT {
+      user: {
+        id: number;
+        email: string;
+        name: string;
+      };
+    }
+  }
+
+  
+
+function buildServer(){
+    const server = Fastify(); 
+
+    server.register(fjwt,{
+        secret: process.env.JWT_SECRET as string,
+    }); 
+
+    server.decorate("authenticate", async function(request: FastifyRequest, reply : FastifyReply){
+        try{
+            await request.jwtVerify(); 
+        }
+        catch(e){
+            return reply.send(e);
+        }
+    }); 
+
+
+    server.addHook("preHandler",(req,res, next)=>{
+        req.jwt = server.jwt; 
+        return next(); 
+    }); 
+
+
+    for(const schema of [...userSchemas]){
+        server.addSchema(schema);
+}
+
+
+ server.register(userRoutes, {prefix : "api/users"});
+
+ return server; 
+};
+
+
+export default buildServer;
