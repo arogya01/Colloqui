87b59bdb8b3f167d87ab1d2b87accffad9f23d30
arogya <arogyabichpuria16@gmail.com>
2024-04-07 20:05:42 +0530

    added login and signup handler

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,59 +3,60 @@ import { CreateUserInput, LoginUserInput } from "./user.schema";
 import { checkExistingUser, createUser } from "./user.service";
 import { verifyPassword } from "../../utils/hash";
 
-
-export const loginHandler = async (request: FastifyRequest<{
-    Body: LoginUserInput
-}>,
-reply : FastifyReply
+export const loginHandler = async (
+  request: FastifyRequest<{
+    Body: LoginUserInput;
+  }>,
+  reply: FastifyReply
 ) => {
-    console.log('loginHandler');
+  console.log("loginHandler");
 
-    const body = request.body; 
+  const body = request.body;
 
-    const user =await checkExistingUser(body.email);
-    console.log('logged-in user, ', user);
+  const user = await checkExistingUser(body.email);
+  console.log("logged-in user, ", user);
 
-    if(!user) return reply.code(401).send({message: 'user not found or invalid user'});
+  if (!user)
+    return reply.code(401).send({ message: "user not found or invalid user" });
 
-    const correctPass = verifyPassword({
-        candidatePassword: body.password, 
-        salt: user.salt, 
-        hash: user.password
-    }); 
+  const correctPass = await verifyPassword({
+    candidatePassword: body.password,
+    salt: user.salt,
+    hash: user.password,
+  });
 
-    if(correctPass){
-        const {password, salt, ...rest } = user; 
+  if (correctPass) {
+    const { password, salt, ...rest } = user;
 
-        // request already has access to jwt?? 
-        // so we can just use it to sign the token, 
-        return { accessToken : request.jwt.sign(rest)}
-    }
+    // request already has access to jwt??
+    // so we can just use it to sign the token,
+    return { accessToken: request.jwt.sign(rest) };
+  }
 
-    return reply.code(401).send({message: 'invalid password'}); 
-
-    
-}
-export async function signupHandler (request: FastifyRequest<{
-    Body : CreateUserInput
-}>, 
-reply: FastifyReply
+  return reply.code(401).send({ message: "invalid password" });
+};
+export async function signupHandler(
+  request: FastifyRequest<{
+    Body: CreateUserInput;
+  }>,
+  reply: FastifyReply
 ) {
- console.log('signup user', request.body);
+  console.log("signup user", request.body);
 
- const body = request.body; 
- 
- if(checkExistingUser(body.email)) return reply.code(400).send({message: 'user already exists'});
+  const body = request.body;
+  const isExistingUser = await checkExistingUser(body.email);
+  if (isExistingUser)
+    return reply.code(400).send({ message: "user already exists" });
 
- try{
-   const user = await createUser(body);
-   return reply.code(201).send(user);
- }
- catch(error){
-    console.log('error occured in singup handler boy', error);
- }
+  try {
+    const user = await createUser(body);
+    console.log("user", user);
+    return reply.code(200).send({ message: "user created successfully" });
+  } catch (error) {
+    console.log("error occured in singup handler boy", error);
+  }
 }
 
-export const getUsersHandler =() => {
-    console.log('loginHandler');
-}
+export const getUsersHandler = () => {
+  console.log("loginHandler");
+};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,49 +2,50 @@ import { hashPassword } from "../../utils/hash";
 import prisma from "../../utils/prisma";
 import { CreateUserInput } from "./user.schema";
 
-export const  createUser = async (requestBody : CreateUserInput) => {
-    const {password , ...input} = requestBody; 
+export const createUser = async (requestBody: CreateUserInput) => {
+  const { password, ...input } = requestBody;
 
-    console.log('creating new user', input);
-    const {hash , salt} = await hashPassword(password);
-    console.log('hash', hash);
-    console.log('salt',salt);
+  console.log("creating new user", input);
+  const { hash, salt } = await hashPassword(password);
+  console.log("hash", hash);
+  console.log("salt", salt);
+
+  try {
     const user = await prisma.user.create({
-        data: {
-            Profile:{
-                create: {
-                    email: input.email, 
-                    salt,
-                    password: hash,
-                    userName: input.userName,
-                    bio: input.bio,
-                    image: input.image, 
-                    phoneNumber : input.phoneNumber
-                    
-                }
-                
-            }
-          // ...other fields...
-        }}); 
-
-        return user; 
-}
-
-export const verifyPassword = async () => {
-   
-}
-
-export const checkExistingUser = async (email : string) => {
-
-    console.log('check for existing user'); 
-
-    const existingUser = await prisma.profile.findUnique({
-      where : {
-        email,
-      }
+      data: {
+        Profile: {
+          create: {
+            email: input.email,
+            salt,
+            password: hash,
+            userName: input.userName,
+            bio: input.bio,
+            image: input.image,
+            phoneNumber: input.phoneNumber,
+          },
+        },
+      },
     });
+    console.log("user is", user);
 
-console.log('exitin user', existingUser);
-    return existingUser; 
+    return user;
+  } catch (error) {
+    console.log("error occured in signup service handler", error);
+    throw error;
+  }
+};
 
-}
+export const verifyPassword = async () => {};
+
+export const checkExistingUser = async (email: string) => {
+  console.log("check for existing user");
+
+  const existingUser = await prisma.profile.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  console.log("exitin user", existingUser);
+  return existingUser;
+};
