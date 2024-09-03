d04349a1ae56c91b75cf59b6f3b25cd4d239848d
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2024-09-03 23:55:46 +0530

    added schema for search fn

---
+++ src/modules/search/search.schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
+
+const searchQuerySchema = z.object({
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  phone: z.string().optional(),
+});
+
+export type SearchQueryInput = z.infer<typeof searchQuerySchema>;
+
+const searchResponseSchema = z.array(
+  z.object({
+    id: z.number(),
+    Profile: z.object({
+      userName: z.string(),
+      email: z.string(),
+      phoneNumber: z.string(),
+      bio: z.string().nullable(),
+      image: z.string().nullable(),
+    }),
+  })
+);
+
+export const { schemas: searchSchemas, $ref } = buildJsonSchemas({
+  searchQuerySchema,
+  searchResponseSchema,
+});
